9017a8ef596ce1694b9d02a20c26baf1d7c6a5e2
Terry Roberts <terryjbroberts@yahoo.com>
2022-07-20 21:38:21 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import NavBar from './components/navigation/NavBar';
+import {useRoutes} from "hookrouter";
 import Home from './components/Home';
 import Books from './components/Books';
-import NavBar from './components/navigation/NavBar';
 import './styles/main.scss';
 
 function App () {
+  
+  const routes = {
+    '/': () => <Home />,
+    '/books': () => <Books />,
+  };
+
+  const routeResult = useRoutes(routes);
+
   return (
     <div className="app-wrapper">
       <NavBar />
-      <Home />
-      <Books />
+      {routeResult}
+
     </div>
   );
 }
--- src/components/navigation/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from '../Home';
-import Books from '../Books';
-
-const routes = {
-  '/': () => <Home />,
-  '/books': () => <Books />,
-};
-
-export default routes;
